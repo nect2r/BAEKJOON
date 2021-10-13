22ee55baf758440e5958acb7ca9a976f80e3eb6c
nect2r <nect2r@kakao.com>
2021-10-13 23:15:31 +0900

    백준 단계별 문제 풀이

---
+++ baekjoon/src/step1/problem_2438.java
@@ -0,0 +1,29 @@
+/*
+ * @since 2021-10-13
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+package step1;
+
+import java.util.Scanner;
+
+public class problem_2438 {
+	public static void main(String[] args){
+    	Scanner scanner = new Scanner(System.in);
+        Integer T = scanner.nextInt();
+        
+        if(1 <= T && T <= 100) {
+        	String star = "*";
+        	
+			for(int i=1; i<=T; i++) {
+				System.out.println(star);
+				star = star + "*";
+			}
+        }
+        scanner.close();
+    }
+}
---
+++ baekjoon/src/step1/problem_8393.java
@@ -0,0 +1,30 @@
+/*
+ * @since 2021-10-13
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+package step1;
+
+import java.util.Scanner;
+
+public class problem_8393 {
+    public static void main(String[] args){
+    	Scanner scanner = new Scanner(System.in);
+        Integer n = scanner.nextInt();
+        Integer count = 0;
+        
+        if(1 <= n && n <= 10000) {
+        	for(int i=1; i<=n; i++) {
+        		count = count + i;
+        	}
+        }else {
+        	System.out.println("n은 1보다 크거나 같아야하며, 10000보다 작거나 같아야합니다.");
+        }
+        
+        System.out.println(count);
+    }
+}
