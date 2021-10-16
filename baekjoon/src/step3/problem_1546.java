639babc6ea275f33da0aa1bc687594d1f9a06fcd
nect2r <nect2r@kakao.com>
2021-10-17 03:18:14 +0900

    1546 1차시도 성공

---
+++ baekjoon/src/step3/problem_1546.java
@@ -0,0 +1,49 @@
+/*
+ * @since 2021-10-17
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+package step3;
+
+import java.util.Scanner;
+
+public class problem_1546 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		//첫번쨰 input은 과목의 갯수 N개
+		int N = in.nextInt();
+		
+		
+		//두번쨰 input은 과목 성적 N개 만큼
+		double[] M = new double[N];
+		
+		//최고점수 H
+		double H = 0;
+
+		//과목의 점수를 저장하면서 최고점수도 갱신함
+		for(int i=0; i<N; i++) {
+			M[i] = in.nextInt();
+			if(M[i] > H) {
+				H = M[i];
+			}
+		}
+		
+		//총점수 SUM
+		double sum = 0;
+		
+		//최고점수로 배열의 전체점수를 다시넣음
+		for(int i=0; i<M.length; i++) {
+			M[i] = M[i]/H*100;
+			sum += M[i];
+		}
+		
+		//결과
+		System.out.println(sum/M.length);
+		in.close();
+	}
+}
