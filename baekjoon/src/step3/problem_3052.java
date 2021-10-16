06d68fb731687b91c69dbce6d69cdc582c38380f
nect2r <nect2r@kakao.com>
2021-10-16 23:26:15 +0900

    백준 코딩

--- baekjoon/src/step3/problem_3052.java
+++ baekjoon/src/step3/problem_3052.java
@@ -1,5 +1,5 @@
 /*
- * @since 2021-10-14
+ * @since 2021-10-16
  * User https://www.acmicpc.net/user/nect2r
  * Blog https://nect2r.tistory.com/
  * Github https://github.com/nect2r/BAEKJOON
@@ -9,32 +9,20 @@
  */
 package step3;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class problem_3052 {
 	public static void main(String[] args) {
 		 
 		Scanner in = new Scanner(System.in);
- 
-		int list[] = {   in.nextInt()%42
-			  			,in.nextInt()%42
-						,in.nextInt()%42
-						,in.nextInt()%42
-						,in.nextInt()%42
-						,in.nextInt()%42
-						,in.nextInt()%42
-						,in.nextInt()%42
-						,in.nextInt()%42
-						,in.nextInt()%42
-					  };
+		HashSet<Integer> h = new HashSet<Integer>();
 		
-		Integer count = 0;
-		
-		for(int o : list) {
-			if(list[0] != o) {
-				count++;
-			}
-			System.out.println(o);
+		for (int i = 0; i < 10; i++) {
+			h.add(in.nextInt() % 42);
 		}
+        
+		in.close();
+		System.out.print(h.size());
 	}
 }
