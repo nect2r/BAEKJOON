409d588779c76add39dea61753760652889fa992
nect2r <nect2r@kakao.com>
2021-10-19 23:23:38 +0900

    야근으로 인해서 1문제 풀고 커밋 ㅠㅠ

--- baekjoon/src/step5/problem_1157.java
+++ baekjoon/src/step5/problem_1157.java
@@ -1,5 +1,5 @@
 /*
- * @since 2021-10-17
+ * @since 2021-10-19
  * User https://www.acmicpc.net/user/nect2r
  * Blog https://nect2r.tistory.com/
  * Github https://github.com/nect2r/BAEKJOON
@@ -14,12 +14,35 @@ import java.util.Scanner;
 public class problem_1157 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String word = in.next();
-		
-		for(int i=0; i<word.length(); i++) {
-			System.out.println(word.charAt(i));
+ 
+		int[] arr = new int[26]; // 영문자의 개수는 26개임
+		String s = in.next();
+ 
+		for (int i = 0; i < s.length(); i++){
+ 
+			if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') { // 대문자 범위
+				arr[s.charAt(i) - 'A']++;	// 해당 인덱스의 값 1 증가
+			}
+    
+			else {	// 소문자 범위
+				arr[s.charAt(i) - 'a']++;
+			}
 		}
-		
-		in.close();
+ 
+ 
+		int max = -1;
+		char ch = '?';
+ 
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = (char) (i + 65); // 대문자로 출력해야하므로 65를 더해준다.
+			}
+			else if (arr[i] == max) {
+				ch = '?';
+			}
+		}
+ 
+		System.out.print(ch);
 	}
 }
