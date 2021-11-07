5d5738e29c8598796576f19a6c0e05f671caed86
nect2r <nect2r@kakao.com>
2021-11-07 18:20:49 +0900

    2021 11 07 백준 코테 연습

---
+++ baekjoon/src/step5/problem_5622.java
@@ -0,0 +1,21 @@
+/*
+ * @since 2021-11-07
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+package step5;
+
+import java.util.*;
+
+public class problem_5622 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String T = in.nextLine();
+		
+		in.close();
+	}
+}
