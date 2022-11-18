085f98577405c07eaebd8271290dde8041f4710e
nect2r <71915361+nect2r@users.noreply.github.com>
2022-11-18 16:53:55 +0900

    18870 좌표 압축은 집가서

---
+++ baekjoon/src/step_10/problem_18870GG.java
@@ -0,0 +1,33 @@
+/*
+ * @since 2022-11-16
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+package step_10;
+
+import java.io.*;
+import java.util.*;
+
+public class problem_18870 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		int[] arr = new int[size];
+
+		int Max = Integer.MIN_VALUE;
+		int Max2 = Integer.MAX_VALUE;
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<size; i++) {
+			int value = Integer.parseInt(st.nextToken());
+			if(value > 0) {
+				
+			}
+			arr[i] = 
+		}
+	}
+}
