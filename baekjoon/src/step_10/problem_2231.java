d30345525ec6e17f153e588cc586d9357de9ac10
nect2r <71915361+nect2r@users.noreply.github.com>
2022-11-22 14:07:52 +0900

    브루트포스 구현법은 이해가 된다 재귀는 너무어려움

---
+++ baekjoon/src/step_10/problem_2231.java
@@ -0,0 +1,25 @@
+/*
+ * @since 2022-11-16
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+package step_10;
+
+import java.io.*;
+import java.util.*;
+
+public class problem_2231 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int answer = 0;
+
+		for(int i=N; i>0; i++) {
+			
+		}
+	}
+}
