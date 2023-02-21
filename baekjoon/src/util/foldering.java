e2d712ad84835e64c88d2f701442e62637c3d5e7
nect2r <71915361+nect2r@users.noreply.github.com>
2023-02-21 14:57:18 +0900

    vscode lib jsoup 추가

---
+++ baekjoon/src/util/foldering.java
@@ -0,0 +1,25 @@
+package util;
+
+
+/*
+ * 백준 단계별로 풀어보기에 맞춰 패키지별로 파일을 구분하고있는데 너무 귀찮아서 제작
+ * 
+ * etc
+ * https://www.acmicpc.net/step 에서 보여주는 단계와 https://www.acmicpc.net/step/? 에서 보여지는 ?인자 url이 다름
+ * 보여주는 단계를 기준으로 정리하도록 진행
+ * 
+ * 종속성
+ * jsoup.*.jar https://jsoup.org/download
+ */
+public class foldering {
+    public static void main(String[] args) {
+        
+    }
+    
+    /*
+     * https://www.acmicpc.net/step 경로의 단계와 경로를 구해온다.
+     */
+    public static void getStepList() {
+
+    }
+}
