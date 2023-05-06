8328053ec9e9a5f7533f4d547401f9310b4fe61d
nect2r <nect2r@kakao.com>
2023-05-06 22:22:14 +0900

    백준 풀이

---
+++ src/step_10/problem_10101.java
@@ -0,0 +1,24 @@
+package step_10;
+
+import java.io.*;
+import java.util.*;
+
+public class problem_10101 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        int y = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        int z = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+
+        if(x % 60 == 0 && y % 60 == 0 && z % 60 == 0) {
+            System.out.println("Equilateral");
+        } else if(180 == x + y + z && (x == y || x == z || y == z)) {
+            System.out.println("Isosceles");
+        } else if(180 == x + y + z) {
+            System.out.println("Scalene");
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
