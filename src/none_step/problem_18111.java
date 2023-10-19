297541e1f0664444df5ce92f0f1f015035f54e7b
nect2r <nect2r@kakao.com>
2023-10-19 15:01:47 +0900

    problem_18111 37364KB, 916ms 로 상대적으로 높아 리팩토링 확인필요

--- src/none_step/problem_18111.java
+++ src/none_step/problem_18111.java
@@ -23,6 +23,9 @@ public class problem_18111 {
         Integer M = Integer.parseInt(st.nextToken());
         Integer B = Integer.parseInt(st.nextToken());
 
+        int MAX_VALUE = Integer.MIN_VALUE;
+        int MIN_VALUE = Integer.MAX_VALUE;
+
         int[][] arr = new int[N][M];
         double sum = 0;
 
@@ -32,9 +35,63 @@ public class problem_18111 {
                 arr[i][j] = Integer.parseInt(st.nextToken());
                 sum += arr[i][j];
 
+                if (arr[i][j] > MAX_VALUE) {
+                    MAX_VALUE = arr[i][j];
+                }
+
+                if (arr[i][j] < MIN_VALUE) {
+                    MIN_VALUE = arr[i][j];
+                }
             }
         }
 
-        //
+        int minTime = Integer.MAX_VALUE;
+        int maxHeight = Integer.MIN_VALUE;
+
+        for (int k = MIN_VALUE; k <= MAX_VALUE; k++ ) {
+
+            int time = 0;
+            int block = B;
+
+            // 기준보다 높다면 층을 낮춘다.
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (arr[i][j] > k) {
+                        int diff = arr[i][j] - k;
+                        block += diff;
+                        time += diff * 2;
+                    }
+                }
+            }
+
+            boolean check = true;
+
+            // 기준보다 낮다면 층을 올린다.
+            loopTop:
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (arr[i][j] < k) {
+                        int diff = k - arr[i][j];
+                        if(block - diff < 0) {
+                            check = false;
+                            break loopTop;
+                        }
+                        block -= diff;
+                        time += diff;
+                    }
+                }
+            }
+
+            if (check) {
+                if (minTime == time && k > maxHeight) {
+                    maxHeight = k;
+                } else if (minTime > time) {
+                    maxHeight = k;
+                    minTime = time;
+                }
+            }
+        }
+
+        System.out.println(minTime + " " + maxHeight);
     }
 }
