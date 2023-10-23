edd57abeee62e385365f1815b8d79e0310876bee
nect2r <71915361+nect2r@users.noreply.github.com>
2023-10-23 17:02:56 +0900

    problem_1463 동적계획법의 활용으로 넘어가서 이전 알고리즘 공부후 재도전

---
+++ src/none_step/problem_1463.java
@@ -0,0 +1,26 @@
+/*
+ * @since 2023-10-23
+ * User https://www.acmicpc.net/user/nect2r
+ * Blog https://nect2r.tistory.com/
+ * Github https://github.com/nect2r/BAEKJOON
+ * Change the class name to Main
+ * Delete the package
+ * Run
+ */
+package none_step;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem_1463 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+    }
+}
